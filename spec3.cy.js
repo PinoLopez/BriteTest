f50cb13400bb29971e7e056eb2f9a6708c2c18e6
Juan Pino <creadoresdigitales@gmail.com>
2024-07-12 16:11:32 +0200

    Add files via upload
    Go to IMDb.com, unfold the Menu button, and navigate to the Top 250 TV Shows
    section; then click on Breaking Bad, go to the Photos, display only Danny Trejo's
    photos, and then click on the 2nd photo in the list.
    I found nobody called Danny Trejo, so used the name Daniel Moncada instead.

---
+++ spec3.cy.js
@@ -0,0 +1,15 @@
+describe('template spec', () => 
+{
+  it('passes', () => 
+  {
+    cy.visit('https://www.imdb.com/')
+    cy.get('.ipc-icon--menu').click()
+    cy.contains('TV Shows').click()
+    cy.contains('Top 250 TV Shows').click()
+    cy.contains('Breaking Bad').click()
+    cy.contains('Photos').click()
+    cy.visit('https://www.imdb.com/title/tt0903747/mediaindex/')
+    cy.visit('https://www.imdb.com/title/tt0903747/mediaindex/?relatedNames=nm3718379')
+    cy.visit('https://www.imdb.com/title/tt0903747/mediaviewer/rm1590360576/?ref_=ttmi_mi_all_2')
+  })
+})
